8e6c6be98a93f685465a5c6bee0f0b8bb62941e1
Blacker <oleg_frolov@mail.ru>
2020-11-27 08:09:06 +0300

    update

--- modules/cms/tpl/assets/js/cms.js
+++ modules/cms/tpl/assets/js/cms.js
@@ -64,7 +64,7 @@ $(document).on("wb-save-done", function (e, params) {
     wbapp.toast("Сохранение","Данные успешно сохранены",{target:'.content-toasts','bgcolor':'success','txcolor':'white'});
 })
 
-$(document).on("ajax-done",function(e,params) {
+$(document).on("wb-ajax-done",function(e,params) {
     $(document).find(".content-body [type=search][data-ajax].search-header").each(function() {
         $(".content-header .content-search [type=search]").attr("data-ajax",$(this).attr("data-ajax")).prop("disabled",false);
         $(this).remove();
