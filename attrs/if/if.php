e1e4360c838b4dc8b236bdc125eab306671200c3
Blacker <oleg_frolov@mail.ru>
2021-03-23 23:30:21 +0300

    wb-if attr fix

--- attrs/if/if.php
+++ attrs/if/if.php
@@ -26,7 +26,7 @@ class attrIf extends wbDom {
           $dom->attr(trim($setattr[0]),$setattr[1]);
         }
       } 
-      if (!$res && !$dom->is('wb-var[else]')) $dom->remove();
+      if (!$res && !$dom->is('wb-var[else]') && !isset($setattr)) $dom->remove();
       
       return $dom;
   }
