d98e2605cd8913d8f371e9d207b170cabb89f6c6
Oleg Frolov <oleg_frolov@mail.ru>
2021-10-25 16:30:36 +0300

    tagsinput

--- modules/tagsinput/tagsinit.js
+++ modules/tagsinput/tagsinit.js
@@ -1,8 +1,9 @@
 $(document).off("tagsinput-js");
-$(document).on("tagsinput-js", function () {
+$(document).on("tagsinput-js", function() {
     var wb_tagsinput = function() {
-        $(".wb-tagsinput").each(function () {
+        $(".wb-tagsinput").each(function() {
             if (this.done == undefined) {
+                let that = this;
                 this.done = true;
                 let placeholder = $(this).attr('placeholder');
                 $(this).tagsInput({
@@ -11,24 +12,25 @@ $(document).on("tagsinput-js", function () {
                     limit: null,
                     placeholder: placeholder,
                     validationPattern: null,
-                    unique: true
+                    unique: true,
+                    onChange: function(ev, data, d1) {
+                        $(that).attr("value", $(this).val());
+                        $(that).trigger('change');
+                    }
                 });
                 $(this).removeClass('wb-tagsinput');
                 $(this).next(".tagsinput").addClass($(this).attr('class'));
-                $(this).on("change",function(){
-                    $(this).attr("value",$(this).val());
-                });
             }
         });
     }
-    setTimeout(function(){
-      wb_tagsinput();
-  },10);
+    setTimeout(function() {
+        wb_tagsinput();
+    }, 10);
 
 });
 
 $(document).off("tagsinit-js");
-$(document).on("tagsinit-js", function () {
+$(document).on("tagsinit-js", function() {
     wbapp.loadScripts(["/engine/modules/tagsinput/tagsinput.js"], "tagsinput-js");
     wbapp.loadStyles(["/engine/modules/tagsinput/tagsinput.css"]);
     $("[data-remove=tagsinit-js]").remove();
