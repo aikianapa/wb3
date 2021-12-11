a344f4ffef6e4fb30d24e8b63fdebfc6a8c1347c
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-12 02:45:50 +0300

    update

--- modules/yonger/tpl/aside_header.inc.php
+++ modules/yonger/tpl/aside_header.inc.php
@@ -1,7 +1,5 @@
-    <a href="{{_route.scheme}}://{{_route.domain}}/workspace" class="aside-logo">
+    <a href="{{_route.scheme}}://{{_route.host}}/workspace" class="aside-logo">
         <img src="/module/yonger/logo" alt="Логотип" />
     </a>
 
-    <svg class="icon logo-menu aside-menu-link mi-menu-burger-square" wb-module="myicons" stroke="#000000">
-        
-    </svg>
+    <svg class="icon logo-menu aside-menu-link mi-menu-burger-square" wb-module="myicons" stroke="#000000"></svg>
