05eed2ece0bf31723b92cf13c35509e29a238ebe
Oleg Frolov <oleg_frolov@mail.ru>
2022-09-21 15:21:24 +0300

    update

--- engine.php
+++ engine.php
@@ -1,12 +1,13 @@
 <?php
+header('Cache-Control: max-age=31536000');
+
 @session_start([
     "gc_probability" => 5,
     "gc_divisor" => 80,
     "gc_maxlifetime" => 84600,
     "cookie_lifetime" => 0
 ]);
-ini_set('display_errors', 1);
-header('Cache-Control: max-age=31536000');
+ini_set('display_errors', 0);
 
 if (!isset($_SESSION["lang"])) $_SESSION["lang"] = "ru";
 
--- modules/editor/editor.php
+++ modules/editor/editor.php
@@ -5,6 +5,7 @@ class modEditor {
       $this->init($dom);
   }
   public function init($dom) {
+	if (!isset($dom->app)) return;
         $editor = $dom->app->vars('_sett.modules.editor.editor');
         $editor > '' ? null : $editor = 'jodit';
         $dom->params->module = $editor;
