fae78a05a26f3f3be30be5a85a6f5b73bb963d83
Oleg Frolov <oleg_frolov@mail.ru>
2022-10-12 02:50:43 +0300

    tree copypaste

--- attrs/tree/tree.js
+++ attrs/tree/tree.js
@@ -1,18 +1,54 @@
 function wb_tree() {
 
-    $.fn.wbTreeInit = function () {
+    $.fn.wbTreeInit = function() {
         let data = $(this).find('.wb-tree-data').html();
         data = json_encode(json_decode(data));
         $(this).find('.wb-tree-data').text(data);
 
-        $(this).find(".wb-tree-line[data-id='']").each(function () {
+        $(this).find(".wb-tree-line[data-id='']").each(function() {
             $(this).attr("data-id", wbapp.newId());
         });
-        $(this).find(".wb-tree-item[data-expand=false]").each(function () {
+        $(this).find(".wb-tree-item[data-expand=false]").each(function() {
             $(this).find("button[data-action=collapse]").trigger("click");
             $(this).removeAttr("data-expand");
         });
-        $(this).delegate(".wb-tree-line", "click", function () {
+        $(this).delegate(".wb-tree-line", "contextmenu", function(ev) {
+            $('#treeLineCopyPasteMenu').remove();
+            let dropdown = `
+            <div class="dropdown dropup pos-absolute mg-l-100" id="treeLineCopyPasteMenu">
+                <div class="dropdown-menu show">
+                    <a class="dropdown-item" href="#copy">Копировать</a>
+                    <a class="dropdown-item" href="#paste">Вставить</a>
+                </div>
+            </div>`
+            $(this).after(dropdown)
+            if (wbapp.data('tree.line.copypaste') == undefined) {
+                $('#treeLineCopyPasteMenu [href="#paste"]').addClass('disabled');
+            }
+            $('#treeLineCopyPasteMenu').parents('.modal').on('click', function() {
+                setTimeout(function() {
+                    $('#treeLineCopyPasteMenu').remove()
+                }, 100)
+            })
+            ev.stopPropagation()
+            return false;
+        });
+
+        $(document).delegate('#treeLineCopyPasteMenu .dropdown-item', wbapp.evClick, function() {
+            let line = $(this).parents('#treeLineCopyPasteMenu').prev(".wb-tree-line");
+            let path = $(line).getPath();
+            if ($(this).attr('href') == '#copy') {
+                let data = $(line).getData(path);
+                wbapp.data('tree.line.copypaste', data.data)
+            }
+            if ($(this).attr('href') == '#paste') {
+                let data = $(line).getData(path);
+                data.data = wbapp.data('tree.line.copypaste')
+                $(line).setData(data, path);
+            }
+            $('#treeLineCopyPasteMenu').remove();
+        })
+        $(this).delegate(".wb-tree-line", "click", function() {
             //wbapp.loading();
             var line = $(this).getLine();
             var item = $(this).getItem();
@@ -31,11 +67,11 @@ function wb_tree() {
             var mid = $(modal).attr("id");
             var modal = $("#" + mid);
             // Procedure after edit branch data //
-            $(modal).on('hidden.bs.modal', function () {
+            $(modal).on('hidden.bs.modal', function() {
                 storeData();
             });
             $(modal).undelegate(".modal-header a.btn:not(.active)", 'click');
-            $(modal).delegate(".modal-header a.btn:not(.active)", 'click', function (ev) {
+            $(modal).delegate(".modal-header a.btn:not(.active)", 'click', function(ev) {
                 storeData(false);
                 let postdict = $(tree).getData(false);
                 let postdata = $(tree).getData(path);
@@ -67,7 +103,7 @@ function wb_tree() {
                     data.data = $(modal).find(".modal-body > .tab-content > .treeData form").serializeJson();
                     $(item).updateItem(data);
                     var store = $(tree).getData(parent);
-                    $.each(store, function (i, d) {
+                    $.each(store, function(i, d) {
                         if (i == lnid) {
                             newdata[data.id] = data;
                         } else {
@@ -82,22 +118,22 @@ function wb_tree() {
             //wbapp.unloading();
         });
 
-        $.fn.dictEvents = function (tree) {
-            $(this).delegate("wb-multiinput", "change", function () {
+        $.fn.dictEvents = function(tree) {
+            $(this).delegate("wb-multiinput", "change", function() {
                 $(tree).setDict($(this).value());
             });
         }
 
-        $(this).delegate(".wb-tree-del", "click", function () {
+        $(this).delegate(".wb-tree-del", "click", function() {
             $(this).delItem();
         });
 
-        $(this).delegate(".wb-tree-add", "click", function () {
+        $(this).delegate(".wb-tree-add", "click", function() {
             var id = $(this).getLine().attr("data-id");
             $(this).addItem(id);
         });
 
-        $(this).delegate(".wb-tree-switch", "click", function () {
+        $(this).delegate(".wb-tree-switch", "click", function() {
             var item = $(this).getItem();
             $(item).children(".wb-tree-check").trigger("click");
             var path = $(this).getPath();
@@ -107,7 +143,7 @@ function wb_tree() {
             $(this).setData(data, path);
         });
 
-        $(this).delegate("button[data-action]", "click", function () {
+        $(this).delegate("button[data-action]", "click", function() {
             var open = true;
             if ($(this).attr("data-action") == "collapse") open = false;
             var item = $(this).getItem();
@@ -118,13 +154,13 @@ function wb_tree() {
         });
 
 
-        $(this).delegate('.wb-tree-swp', 'mousedown', function () {
+        $(this).delegate('.wb-tree-swp', 'mousedown', function() {
             var item = $(this).getItem();
             $(item).data("move", { "path": $(this).getPath(), "indx": $(item).index() });
             $(item).addClass("wb-tree-drag");
         });
 
-        $(this).on('change', function () {
+        $(this).on('change', function() {
             // change branch position //
             $(this).find(".wb-tree ")
             var drag = $(this).find(".wb-tree-item.wb-tree-drag");
@@ -144,14 +180,14 @@ function wb_tree() {
             var newdata = {};
             if (path == move.path) {
                 // Inside branch //
-                $(drag).parent("ol").find(".wb-tree-line").each(function () {
+                $(drag).parent("ol").find(".wb-tree-line").each(function() {
                     var id = $(this).attr("data-id");
                     newdata[id] = store[id];
                 });
                 $(tree).setData(newdata, parent);
             } else {
                 // Outeside branch //
-                $(drag).parent("ol").find(".wb-tree-line").each(function () {
+                $(drag).parent("ol").find(".wb-tree-line").each(function() {
                     var id = $(this).attr("data-id");
                     if (id == selfdata.id) {
                         newdata[id] = selfdata;
@@ -164,13 +200,13 @@ function wb_tree() {
                 $(tree).delData(move.path);
             }
         });
-        $(this).on("wb-tree-change-data", function () {
+        $(this).on("wb-tree-change-data", function() {
             //console.log($(this).val().data);
         });
         $(this).disableSelection();
     }
 
-    $.fn.setData = function (newdata, path = false) {
+    $.fn.setData = function(newdata, path = false) {
 
         var tree = $(this).getTree();
         if (path === false) {
@@ -187,7 +223,7 @@ function wb_tree() {
         $(this).trigger("wb-tree-change-data");
     }
 
-    $.fn.setDict = function (newdict, path = false) {
+    $.fn.setDict = function(newdict, path = false) {
         var tree = $(this).getTree();
         var data = $(this).getData(true);
         eval("data.dict = newdict;");
@@ -198,17 +234,17 @@ function wb_tree() {
         $(this).trigger("wb-tree-change-dict");
     }
 
-    $.fn.updateItem = function (data) {
+    $.fn.updateItem = function(data) {
         var tree = $(this).getTree();
         var item = this;
-        wbapp.post("/ajax/tree/update", data, function (data) {
+        wbapp.post("/ajax/tree/update", data, function(data) {
             $(item).children(":not(ol)").remove();
             $(item).prepend($(data.content).html());
             $(tree).nestable('refresh');
         });
     }
 
-    $.fn.addItem = function (id, copy = false) {
+    $.fn.addItem = function(id, copy = false) {
         var root = false;
         var item = $(this).getItem();
         var parent = $(this).getParentPath(true);
@@ -228,7 +264,7 @@ function wb_tree() {
                 line["id"] = copy;
             }
         }
-        $.each(data, function (i, d) {
+        $.each(data, function(i, d) {
             newdata[d.id] = d;
             if (d.id == id) {
                 newdata[line["id"]] = line;
@@ -241,7 +277,7 @@ function wb_tree() {
         $(item).after(newitem);
     }
 
-    $.fn.delItem = function () {
+    $.fn.delItem = function() {
         var tree = $(this).getTree();
         var item = $(this).getItem();
         var path = $(this).getPath();
@@ -260,7 +296,7 @@ function wb_tree() {
         }
     }
 
-    $.fn.getTree = function () {
+    $.fn.getTree = function() {
         if ($(this).hasClass("wb-tree")) {
             return this;
         } else {
@@ -268,7 +304,7 @@ function wb_tree() {
         }
     }
 
-    $.fn.getItem = function () {
+    $.fn.getItem = function() {
         if ($(this).hasClass(".wb-tree-item")) {
             return this;
         } else {
@@ -276,7 +312,7 @@ function wb_tree() {
         }
     }
 
-    $.fn.getLine = function () {
+    $.fn.getLine = function() {
         if ($(this).hasClass("wb-tree-line")) {
             return this;
         } else if ($(this).hasClass("wb-tree-item")) {
@@ -286,7 +322,7 @@ function wb_tree() {
         }
     }
 
-    $.fn.getPath = function () {
+    $.fn.getPath = function() {
         var line = $(this).getLine();
         var path = $(line).attr("data-id");
         var stop = false;
@@ -305,7 +341,7 @@ function wb_tree() {
         return path;
     }
 
-    $.fn.getParentPath = function (branch = false) {
+    $.fn.getParentPath = function(branch = false) {
         var line = $(this).getLine();
         var parent = $(line).parent(".wb-tree-item").parent("ol");
         if (!$(parent).hasClass("wb-tree")) {
@@ -318,7 +354,7 @@ function wb_tree() {
         return parent;
     }
 
-    $.fn.getData = function (path = true) {
+    $.fn.getData = function(path = true) {
         // true - return data+dict
         // false - return dict only
         // path - return data only
@@ -338,7 +374,7 @@ function wb_tree() {
         return data;
     }
 
-    $.fn.delData = function (path) {
+    $.fn.delData = function(path) {
         var tree = $(this).getTree();
         var content = $(tree).getData();
         var data = $(tree).getData(path);
@@ -363,15 +399,16 @@ function wb_tree() {
 
     function convPath(path) {
         path = path.split(".");
-        $.each(path, function (i, v) {
-            if (v !== '["children"]') v = '["' + v + '"]'; path[i] = v;
+        $.each(path, function(i, v) {
+            if (v !== '["children"]') v = '["' + v + '"]';
+            path[i] = v;
         });
         path = path.join("");
         return path;
     }
 
-    $(document).on("nestable-js", function () {
-        $(".wb-tree").each(function (e) {
+    $(document).on("nestable-js", function() {
+        $(".wb-tree").each(function(e) {
             if (this.done == undefined) {
                 this.done = true;
                 $(this).nestable({
@@ -388,15 +425,16 @@ function wb_tree() {
             }
         });
     });
-    
+
     wbapp.loadStyles(["/engine/attrs/tree/tree.less",
         "/engine/lib/js/nestable/nestable.css",
-        "/engine/lib/fonts/remixicons/remixicon.less"]);
+        "/engine/lib/fonts/remixicons/remixicon.less"
+    ]);
     wbapp.loadScripts(["/engine/lib/js/nestable/nestable.min.js"], "nestable-js");
 
 
 }
 
-$(document).on("wb-tree-js", function () {
+$(document).on("wb-tree-js", function() {
     wb_tree();
 });
