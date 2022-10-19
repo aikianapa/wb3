0ade095c829efd88a8a8e725f3741c6ad58650a8
Oleg Frolov <oleg_frolov@mail.ru>
2022-10-19 10:54:59 +0300

    langinp mod

--- modules/langinp/langinp_mod.js
+++ modules/langinp/langinp_mod.js
@@ -1,41 +1,44 @@
-        setTimeout(function() {
-            $('.mod-langinp[data-mid]').each(function() {
-                let mod = this
-                let id = $(mod).attr('id');
-                if (id == undefined) {
-                    id = $(mod).attr('data-mid')
-                    $(mod).attr('id', id)
-                }
-                $(mod).removeAttr('data-mid')
-                let ractive = new Ractive({
-                    el: '#' + id,
-                    template: $('#' + id).html(),
-                    data: {},
-                    on: {
-                        init() {
-                            try {
-                                this.set(json_decode($(mod).find('textarea.mod-langinp-data').html()))
-                            } catch (error) {
-                                null
-                            }
-                        },
-                        render() {
-                            $.each(this.get(), function(lng, val) {
-                                $(mod).find(`[data-lang="${lng}"]:input`).val(val)
-                            })
-                        },
-                        dropdown(ev) {
-                            let form = $(mod).find('.dropdown-menu');
-                            let width = $(ev.node).closest('.input-group').width();
-                            $(form).width(width);
-                        },
-                        edit(ev) {
-                            let lng = $(ev.node).data('lang')
-                            ractive.set(lng, $(ev.node).val())
-                            ractive.fire('render')
-                            $(mod).find('textarea.mod-langinp-data').html(json_encode(ractive.get())).trigger('change')
+$(document).off('langinp-mod-js');
+$(document).on('langinp-mod-js', function() {
+    setTimeout(function() {
+        $('.mod-langinp[data-mid]').each(function() {
+            let mod = this
+            let id = $(mod).attr('id');
+            if (id == undefined) {
+                id = $(mod).attr('data-mid')
+                $(mod).attr('id', id)
+            }
+            $(mod).removeAttr('data-mid')
+            let ractive = new Ractive({
+                el: '#' + id,
+                template: $('#' + id).html(),
+                data: {},
+                on: {
+                    init() {
+                        try {
+                            this.set(json_decode($(mod).find('textarea.mod-langinp-data').html()))
+                        } catch (error) {
+                            null
                         }
+                    },
+                    render() {
+                        $.each(this.get(), function(lng, val) {
+                            $(mod).find(`[data-lang="${lng}"]:input`).val(val)
+                        })
+                    },
+                    dropdown(ev) {
+                        let form = $(mod).find('.dropdown-menu');
+                        let width = $(ev.node).closest('.input-group').width();
+                        $(form).width(width);
+                    },
+                    edit(ev) {
+                        let lng = $(ev.node).data('lang')
+                        ractive.set(lng, $(ev.node).val())
+                        ractive.fire('render')
+                        $(mod).find('textarea.mod-langinp-data').html(json_encode(ractive.get())).trigger('change')
                     }
-                })
-            }, 200)
-        })
+                }
+            })
+        }, 200)
+    })
+})
