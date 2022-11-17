2f7cb25b53c5a146bb9fec23904001ef52869994
Oleg Frolov <oleg_frolov@mail.ru>
2022-11-18 01:20:14 +0300

    tree icons svg

--- attrs/tree/tree_ui.php
+++ attrs/tree/tree_ui.php
@@ -1,9 +1,14 @@
 <ol class='wb-tree'>
     <li class='wb-tree-item dd-item' data-expand="{{open}}">
-    <i class="wb-tree-swp py-1"><img src="/module/myicons/24/3b6998/arrows-diagrams-02.svg"></i>
-  <i class="wb-tree-del py-1"><img src="/module/myicons/24/3b6998/interface-essential-306.svg"></i>
-  <i class="wb-tree-add py-1"><img src="/module/myicons/24/3b6998/interface-essential-228.svg"></i>
-
+    <i class="wb-tree-swp py-1">
+        <svg class="mi mi-arrows-diagrams-02" size="24" stroke="3b6998" wb-on wb-module="myicons"></svg>
+    </i>
+  <i class="wb-tree-del py-1">
+        <svg class="mi mi-interface-essential-306" size="24" stroke="3b6998" wb-on wb-module="myicons"></svg>
+  </i>
+  <i class="wb-tree-add py-1">
+    <svg class="mi mi-interface-essential-228" size="24" stroke="3b6998" wb-on wb-module="myicons"></svg>
+  </i>
         <div class='wb-tree-line form-control' data-id="{{id}}">{{name}}</div>
         <label class="wb-tree-switch"><input wb-module="switch" class="wb-tree-check" name="active"><span></span></label>
     </li>
