12eeb88713985c92085f37ae585684acaa7551d0
Oleg Frolov <oleg_frolov@mail.ru>
2022-12-19 16:02:41 +0300

    pagination ios fix

--- tags/pagination/pagination.js
+++ tags/pagination/pagination.js
@@ -10,14 +10,32 @@ $(document).one("pagination-js", function() {
 
         if (selector !== undefined) {
             $(document).find(selector).on(wbapp.evClick, function() {
-                $(more).find(".page-link").trigger("click");
+                $(more).find(".page-link").trigger(wbapp.evCkick);
             });
         }
     });
 
     $(document).on('scroll', function(ev) {
         $(document).find(".pagination .page-more[data-trigger=auto]").each(function() {
-            is_visible(this) ? $(this).find(".page-link").trigger("click") : null;
+            let elem = $(this)[0]
+            if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+            const style = getComputedStyle(elem);
+            if (style.display === 'none') return false;
+            if (style.visibility !== 'visible') return false;
+            if (style.opacity < 0.1) return false;
+            if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+                elem.getBoundingClientRect().width === 0) {
+                return false;
+            }
+            const elemCenter = {
+                x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+                y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+            };
+            if (elemCenter.x < 0) return false;
+            if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+            if (elemCenter.y < 0) return false;
+            if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+            $(this).find(".page-link").trigger("tap")
         })
     });
 
