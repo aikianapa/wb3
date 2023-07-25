9e242f45b5cb8c1c98536c096db2df7e29e2c7e3
Олег Фролов <oleg_frolov@mail.ru>
2023-07-25 23:16:08 +0300

    cache-control

--- controllers/form.php
+++ controllers/form.php
@@ -18,17 +18,17 @@ class ctrlForm
         $item = $this->app->vars('_route.item');
         $tpl = $this->app->vars('_route.tpl');
         $class = $this->app->formClass($form);
-        if (method_exists($class,$mode)) {
+        if (method_exists($class, $mode)) {
             echo $class->$mode();
             exit;
         } else {
-            if ($tpl > '')  {
+            if ($tpl > '') {
                 $out = $this->app->getTpl($tpl);
             } else {
-                $out = $this->app->getForm($form,$mode);
+                $out = $this->app->getForm($form, $mode);
             }
             if ($out) {
-                if ($item > '') $out->item = $this->app->itemRead($form,$item);
+                if ($item > '') $out->item = $this->app->itemRead($form, $item);
                 $out->fetch();
                 echo $out->outer();
                 exit;
@@ -47,12 +47,12 @@ class ctrlForm
         $app = &$this->app;
         $cache = $app->getCache();
         $item = [];
-        if ($app->vars('_post._tid') > '' AND $app->vars('_post._tid') == $app->vars('_post.target') AND $app->vars('_post.filter') > '') {
+        if ($app->vars('_post._tid') > '' and $app->vars('_post._tid') == $app->vars('_post.target') and $app->vars('_post.filter') > '') {
             // признак фильрации в темплейте
             $this->target = $app->vars('_post.target');
             $cache = false;
         }
-        if ($cache === false OR $cache === null) {
+        if ($cache === false or $cache === null or !$cache) {
             $_ENV["cache_used"] = false;
             $dom = isset($app->route->form) ? $app->getForm($app->route->form, $app->route->mode) : null;
             if ($dom->error) $dom = null;
@@ -60,9 +60,9 @@ class ctrlForm
                 $table = $app->route->form;
                 isset($app->route->table) ? $table = $app->route->table : null;
                 $item = $app->itemRead($table, $app->route->item);
-                if ($item && !isset($item['active']) OR $item['active'] == 'on') {
-                    $item = wbTrigger('form', __FUNCTION__, 'beforeItemShow', [$table], $item);                    
-                    if (!$dom && (!isset($item['template']) OR $item['template'] == '') AND $app->vars('_route.tpl') > '') {
+                if ($item && !isset($item['active']) or $item['active'] == 'on') {
+                    $item = wbTrigger('form', __FUNCTION__, 'beforeItemShow', [$table], $item);
+                    if (!$dom && (!isset($item['template']) or $item['template'] == '') and $app->vars('_route.tpl') > '') {
                         $dom = $app->getTpl($app->vars('_route.tpl'));
                     } elseif (!$dom && isset($item['template']) and $item['template'] > '') {
                         $dom = $app->getTpl($item['template']);
@@ -70,8 +70,8 @@ class ctrlForm
                     if (!$dom && isset($app->route->tpl)) {
                         $dom = $app->getTpl($app->route->tpl);
                     }
-                    if (!$dom ) {
-                        $app->route->tpl = $table.'-show.php';
+                    if (!$dom) {
+                        $app->route->tpl = $table . '-show.php';
                         $dom = $app->getTpl($app->route->tpl);
                     }
                     if (!$dom) {
@@ -82,7 +82,7 @@ class ctrlForm
                     }
                     $dom ? $dom->item = $item : null;
                 }
-            } else if ($app->vars('_route.tpl') >'' ) {
+            } else if ($app->vars('_route.tpl') > '') {
                 $dom = $app->getTpl($app->vars('_route.tpl'));
             }
             if (!$dom or (isset($item['active']) and $item['active'] !== 'on')) {
@@ -93,8 +93,8 @@ class ctrlForm
 
             if ($app->vars('_sett.devmode') == 'on') {
                 $scripts = $dom->find('script[src]:not([src*="?"])');
-                foreach($scripts as $script) {
-                    $src = $script->attr('src').'?'.wbNewId();
+                foreach ($scripts as $script) {
+                    $src = $script->attr('src') . '?' . wbNewId();
                     $script->attr('src', $src);
                 }
             }
@@ -102,19 +102,18 @@ class ctrlForm
             $ttls = $dom->find('title');
             $title_prepend = $app->vars('_var.title_prepend');
             foreach ($ttls as $t) {
-                $dom->find('head title')->inner(trim($title_prepend.' '.$t->text()));
+                $dom->find('head title')->inner(trim($title_prepend . ' ' . $t->text()));
             }
             $dom->find('body body')->remove();
-            $out = isset($this->target) ? '<div>'.$dom->find($this->target)->outer().'</div>' : $out = $dom->outer();
+            $out = isset($this->target) ? '<div>' . $dom->find($this->target)->outer() . '</div>' : $out = $dom->outer();
             //$out = $this->app->fromString($out);
             //$this->app->module('compress',$out);
             //$out = $out->outer();
-            !strpos(' '.trim($out), '<!DOCTYPE') ? $out = '<!DOCTYPE html>'.$out : null;
+            !strpos(' ' . trim($out), '<!DOCTYPE') ? $out = '<!DOCTYPE html>' . $out : null;
             is_callable('beforeShow') ? $out = beforeShow($out) : null;
             $app->vars('_route.tpl') == '404.php' ? header($_SERVER['SERVER_PROTOCOL'] . " 404 Not Found", true) : null;
             echo $out;
             $app->setCache($out);
-
         } else {
             $_ENV["cache_used"] = true;
             ob_get_contents();
@@ -143,23 +142,23 @@ class ctrlForm
         $app = $this->app;
         $form = $app->vars('_route.params.0');
         $mode = $app->vars('_route.params.1');
-        if ($mode == 'list' AND $app->vars('_post.render') == 'client') {
-//            $dom = $app->getForm($form, $mode);
-//            $dom->fetch();
-            $options = ( object )$_POST;
-            !isset($options->size) ? $options->size = 500 : 0 ;
+        if ($mode == 'list' and $app->vars('_post.render') == 'client') {
+            //            $dom = $app->getForm($form, $mode);
+            //            $dom->fetch();
+            $options = (object)$_POST;
+            !isset($options->size) ? $options->size = 500 : 0;
             !isset($options->page) ? $options->page = 1 : 0;
             !isset($options->filter) ? $options->filter = [] : 0;
-            $list = $app->itemList($form, ( array )$options);
+            $list = $app->itemList($form, (array)$options);
             foreach ($list['list'] as &$item) {
-                if (isset($_ENV['locale']) AND isset($_ENV['locale'][$_ENV['lang']])) {
+                if (isset($_ENV['locale']) and isset($_ENV['locale'][$_ENV['lang']])) {
                     $item['_lang'] = &$_ENV['locale'][$_ENV['lang']];
                 }
                 $item = wbTrigger('form', __FUNCTION__, 'beforeItemShow', [$form], $item);
             }
             $pages = ceil($list['count'] / $options->size);
             $pagination = wbPagination($options->page, $pages);
-            echo json_encode(['result'=>$list['list'], 'pages'=>$pages, 'page'=>$options->page, 'pagination'=>$pagination]);
+            echo json_encode(['result' => $list['list'], 'pages' => $pages, 'page' => $options->page, 'pagination' => $pagination]);
             exit;
         }
     }
